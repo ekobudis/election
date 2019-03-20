9d78e16f9a173e9637fea76b0e09d47a0a9fa7d5
Visca Marisca <vimasolusi@gmail.com>
2019-03-20 23:30:08 +0700

    Change Access Token s

--- app/src/main/java/com/vimasolusi/election/network/ApiService.java
+++ app/src/main/java/com/vimasolusi/election/network/ApiService.java
@@ -1,16 +1,6 @@
 package com.vimasolusi.election.network;
 
-import com.vimasolusi.election.model.AccessToken;
-
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.POST;
 
 public interface ApiService {
-
-    @POST("login")
-    @FormUrlEncoded
-
-    Call<AccessToken> login(@Field("nrp") String nrp, @Field("password") String password);
 
 }
